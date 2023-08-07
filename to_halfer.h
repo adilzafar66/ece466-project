4a58723dd0b08dc0a5088cd33f7598a6861d2cc9
Adil <adilzafar66@gmail.com>
2023-08-06 21:49:20 -0700

    oo

--- to_halfer.h
+++ to_halfer.h
@@ -8,7 +8,9 @@ SC_MODULE (to_half)
 	sc_in<NN_DIGIT> input_to_hh;
 	sc_out<NN_DIGIT> output_to_hh;
 
-	void to_high_half_process();
+	void to_high_half_process() {
+		output_to_hh.write(TO_HIGH_HALF(input_to_hh.read()));
+	}
 
 	SC_CTOR (to_half)
 	{
@@ -16,11 +18,5 @@ SC_MODULE (to_half)
 		sensitive << input_to_hh;
 	}
 };
-
-void to_half::to_high_half_process()
-{
-	output_to_hh.write(TO_HIGH_HALF(input_to_hh.read()));
-}
-
 
 #endif //TO_HALFER_H

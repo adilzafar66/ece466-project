851ee69a7ec8b20c1694edac8c5b6701b547f88d
Adil <adilzafar66@gmail.com>
2023-08-06 21:24:33 -0700

    oo

--- dh_sw.cpp
+++ dh_sw.cpp
@@ -498,7 +498,7 @@ void dh_sw::NN_DigitDivHH_Ref(
     }
 
     t[1] -= HIGH_HALF(u);
-    std::cout << "Software T1: " <<  t[1] << std::endl;
+    std::cout << "Software T1: " <<  HIGH_HALF(u) << std::endl;
     t[1] -= v;
 
     while ((t[1] > cHigh) || ((t[1] == cHigh) && (t[0] >= TO_HIGH_HALF(cLow))))
